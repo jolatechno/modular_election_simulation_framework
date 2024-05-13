74f5f94ad87c556afc7341d70526e604a095a0ee
jolatechno <joseph.touzet@ens-paris-saclay.fr>
2024-05-13 11:31:22 +0200

    update

--- src/core/segregation/multiscalar.hpp
+++ src/core/segregation/multiscalar.hpp
@@ -51,7 +51,7 @@ namespace segregation::multiscalar {
 		return KLdiv_trajectories;
 	}
 
-	std::vector<std::vector<size_t>> get_focal_distance_indexes(const std::vector<std::vector<double>> &KLdiv_trajectories, const std::vecot<double> &convergence_coefficients) {
+	std::vector<std::vector<size_t>> get_focal_distance_indexes(const std::vector<std::vector<double>> &KLdiv_trajectories, const std::vector<double> &convergence_coefficients) {
 		std::vector<std::vector<size_t>> focal_distance_indexes(KLdiv_trajectories.size(), std::vector<size_t>(convergence_coefficients.size()));
 
 		/* TODO */
@@ -60,16 +60,25 @@ namespace segregation::multiscalar {
 	}
 
 	template<typename Type>
-	std::vector<std::vector<Type>> get_focal_distances(const std::vector<std::vector<double>> &KLdiv_trajectories, const std::vecot<double> &convergence_coefficients, const std::vector<Type> &Xvalues) {
+	std::vector<std::vector<Type>> get_focal_distances(const std::vector<std::vector<double>> &KLdiv_trajectories, const std::vector<double> &convergence_coefficients, const std::vector<Type> &Xvalues) {
 		std::vector<std::vector<size_t>> focal_distance_indexes = get_focal_distance_indexes(KLdiv_trajectories, convergence_coefficients);
 
-		std::vector<std::vector<Type>> focal_distances(trajectories[0].size(), std::vector<double>(trajectories[0].size()));
-		for (size_t i = KLdiv_trajectories.size(); ++i) {
+		std::vector<std::vector<Type>> focal_distances(KLdiv_trajectories.size(), std::vector<double>(convergence_coefficients.size()));
+		for (size_t i = 0; KLdiv_trajectories.size(); ++i) {
 			for (size_t j = 0; j < KLdiv_trajectories[i].size(); ++j) {
 				focal_distances[i][j] = Xvalues[focal_distance_indexes[i][j]];
 			}
 		}
 		
 		return focal_distances;
+	}
+
+	template<typename Type>
+	std::vector<Type> get_distortion_coefs(const std::vector<std::vector<Type>> &focal_distances, const std::vector<Type> &Xvalues, const Type &normalization_coef=1.0) {
+		std::vector<Type> distortion_coefs(focal_distances.size());
+
+		/* TODO */
+		
+		return distortion_coefs;
 	}
 }
