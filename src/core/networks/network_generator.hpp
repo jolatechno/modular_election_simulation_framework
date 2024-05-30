0c7ad9897e6d54114421ea1f42aa8ca2581059ae
jo <joseph.touzet@ens-paris-saclay.fr>
2024-05-31 00:38:20 +0200

    update

--- src/core/networks/network_generator.hpp
+++ src/core/networks/network_generator.hpp
@@ -6,6 +6,7 @@
 #include "../network.hpp"
 
 #include "../../util/util.hpp"
+#include "../../util/math.hpp"
 
 
 namespace BPsimulation::random {
@@ -48,6 +49,40 @@ namespace BPsimulation::random {
 				}
 				degrees[node]  = node_degree_save + 1;
 				total_degree  += node_degree_save + 2;
+			}
+		}
+	}
+
+	template<class Agent, class Type>
+	void closest_neighbor_limited_attachment(SocialNetwork<Agent> *network, const std::vector<std::vector<Type>> &distances,
+		const int n_attachment, const int n_attachment_max=0)
+	{
+		const int n_attachment_max_ = std::max(n_attachment_max, n_attachment);
+
+		std::vector<size_t>              idxs(          network->num_nodes(), 0);
+		std::vector<std::vector<size_t>> sorted_indexes(network->num_nodes());
+		for (size_t node = 0; node < network->num_nodes(); ++node) {
+			sorted_indexes[node] = util::math::get_sorted_indexes(distances[node]);
+		}
+
+		auto nodes = network->nodes();
+		std::shuffle(nodes.begin(), nodes.end(), util::get_random_generator());
+
+		for (int i = 0; i < n_attachment; ++i) {
+			for (size_t node : nodes) {
+				if (network->neighbors(node).size() < n_attachment) {
+					while (idxs[node] < network->num_nodes()-1 && (
+						   node == sorted_indexes[node][idxs[node]] ||
+						   network->neighbors(sorted_indexes[node][idxs[node]]).size() >= n_attachment_max_))
+					{
+						++idxs[node];
+					}
+
+					if (idxs[node] < network->num_nodes()) {
+						network->add_connection(node, sorted_indexes[node][idxs[node]]);
+						++idxs[node];
+					}
+				}
 			}
 		}
 	}
